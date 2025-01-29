0effb36cd1f35091d396562aeea9ba6347cc710a
sai sandeep <sandeepsai530@gmail.com>
2025-01-29 18:42:17 +0530

    added 04-variables-password

--- 04-variables-password.sh
+++ 04-variables-password.sh
@@ -6,6 +6,7 @@ echo "entered username: $USERNAME1"
 echo "please enter your username2: "
 read -s USERNAME2
 echo "entered username: $USERNAME2"
+echo "please enter password2:"
 read -s PASSWORD2
 
 
