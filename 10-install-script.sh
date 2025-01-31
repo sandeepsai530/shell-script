3eb8328ab55296f30c756880dbd3c3142b764945
sai sandeep <sandeepsai530@gmail.com>
2025-01-31 19:38:50 +0530

    10 script added

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -8,4 +8,18 @@ then
     exit 1 #other than 0
 fi
 
-dnf ;ist installed mysql
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then 
+        echo "Installing MYSQL ... FAILURE"
+        exit 1
+    else
+        echo " Installing MYSQL ... SUCCESS"
+    fi
+else
+echo "MYSQL is already INSTALLED"
+fi
