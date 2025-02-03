e250dd1958fbf76c6167574250911bf6da5ce170
sai sandeep <sandeepsai530@gmail.com>
2025-02-03 17:20:49 +0530

    13 script added

--- 13-logs.sh
+++ 13-logs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-USER=$(id -u)
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -9,42 +9,43 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE_NAME=$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
-    then 
-        echo -e "$2 ... $R FAILURE"
+    then
+        echo -e "$2 ... $R FAILURE $N"
         exit 1
     else
-        echo -e "$2 ... $G SUCCESS"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
-echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
-if [ $USER -ne 0 ]
+if [ $USERID -ne 0 ]
 then
-    echo "ERROR: unauthorized user"
+    echo "ERROR:: You must have sudo access to execute this script"
     exit 1 #other than 0
 fi
 
 dnf list installed mysql &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
-then 
+then # not installed
     dnf install mysql -y &>>$LOG_FILE_NAME
-    VALIDATE $? "installing MYSQL"
+    VALIDATE $? "Installing MySQL"
 else
-    echo -e "MYSQL is already ...  $Y INSTALLED"
+    echo -e "MySQL is already ... $Y INSTALLED $N"
 fi
+
 
 dnf list installed git &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
-then # not installed
+then
     dnf install git -y &>>$LOG_FILE_NAME
-    VALIDATE $? "Installing GIT"
+    VALIDATE $? "Installing Git"
 else
-    echo -e "GIT is already $Y INSTALLED"
+    echo -e "Git is already ... $Y INSTALLED $N"
 fi
