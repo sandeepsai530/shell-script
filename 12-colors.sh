85ecf0694af863a7824465b3f1539046da610124
sai sandeep <sandeepsai530@gmail.com>
2025-02-03 15:37:23 +0530

    12 script added

---
+++ 12-colors.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USER=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then 
+    echo "$2 ... $R FAILURE"
+    exit 1
+else
+    echo "$2 ... $G SUCCESS"
+fi
+}
+
+if [ $USER -ne 0 ]
+then
+    echo "ERROR: unauthorized user"
+    exit 1 #other than 0
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y
+    VALIDATE $? "$Y installing MYSQL"
+else
+    echo "$Y MYSQL is already INSTALLED"
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then # not installed
+    dnf install git -y
+    VALIDATE $? "$Y Installing GIT"
+else
+    echo "GIT is already $G INSTALLED"
+fi
