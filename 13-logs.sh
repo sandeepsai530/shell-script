536993bf821209f8c4b004cbca642d6deb646aba
sai sandeep <sandeepsai530@gmail.com>
2025-02-03 16:52:31 +0530

    12 script added

--- 13-logs.sh
+++ 13-logs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 USER=$(id -u)
-LOGS_FOLDER="/var/log/shellscript-logs"
-LOG_FILE=$(echo $0 | cut -d "." -f1 )
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE_NAME=$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+# LOGS_FOLDER="/var/log/shellscript-logs"
+# LOG_FILE=$(echo $0 | cut -d "." -f1 )
+# TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+# LOG_FILE_NAME=$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
