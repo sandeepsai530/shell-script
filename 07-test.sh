e4e37cdc83097633d369598a2f2a8c4bedf97904
sai sandeep <sandeepsai530@gmail.com>
2025-02-03 14:58:12 +0530

    07 script added

--- 07-test.sh
+++ 07-test.sh
@@ -3,26 +3,26 @@
 USER=$(id -u)
 
 if [ $USER -eq 0 ]
-then
+then 
     echo "he is ROOT user"
 else
-    echo "he is NOT A ROOT user"
+    echo "he is NOT a ROOT user"
 fi
 
 dnf list installed mysql
 
 if [ $? -ne 0 ]
-then # not installed
+then 
     dnf install mysql -y
     else
-        echo "MYSQL installed SUCCESS"
+        echo " MYSQL already installed"
 fi
 
 dnf list installed git
 
 if [ $? -ne 0 ]
-then # not installed
+then
     dnf install git -y
     else
-        echo "GIT installed SUCCESS"
+        echo "GIT already installed"
 fi
