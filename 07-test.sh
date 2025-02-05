1996a1e5eb818ad3b368b61f506e83dec1c24e23
sai sandeep <sandeepsai530@gmail.com>
2025-02-05 20:21:33 +0530

    18 script added

--- 07-test.sh
+++ 07-test.sh
@@ -14,6 +14,16 @@ LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
 CHECK_ROOT(){
     if [ USERID -ne 0 ]
     then
