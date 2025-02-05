714381ad5bfef54adce9b5ccf74fa3df2791fe72
sai sandeep <sandeepsai530@gmail.com>
2025-02-05 18:00:22 +0530

    18 script added

--- 18-backup.sh
+++ 18-backup.sh
@@ -33,4 +33,17 @@ if [ $# -lt 2 ]
 then
     USAGE
 fi
+
+if [ ! -d $SOURCE_DIR ]
+then
+    echo -e "$SOURCE_DIR does not exist... please check"
+    exit 1
+fi
+
+if [ ! -d $DEST_DIR ]
+then
+    echo -e "$DEST_DIR does not exist... please check"
+    exit 1
+fi
+
 echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
