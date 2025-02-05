5abb8c75469d64f22be20edbe9d583f1792312dc
sai sandeep <sandeepsai530@gmail.com>
2025-02-05 18:13:42 +0530

    18 script added

--- 18-backup.sh
+++ 18-backup.sh
@@ -50,3 +50,10 @@ echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 echo "Files are: $FILES"
+
+if [ -n $FILES ]
+then
+    echo "Files are: $FILES"
+else
+    echo "No files found older than $DAYS"
+fi
