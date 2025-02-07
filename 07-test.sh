c03e4ebb28ad5768bc32e5f6884eac4cb85f6cac
sai sandeep <sandeepsai530@gmail.com>
2025-02-07 19:36:04 +0530

    18 script added

--- 07-test.sh
+++ 07-test.sh
@@ -4,6 +4,11 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
 USERID=$(id -u)
 
 CHECK_ROOT(){
@@ -16,3 +21,4 @@ fi
 }
 
 CHECK_ROOT
+
