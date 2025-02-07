7fdc410448b899357c0f8e1fcbf622d1d40e470f
sai sandeep <sandeepsai530@gmail.com>
2025-02-07 19:57:55 +0530

    18 script added

--- 07-test.sh
+++ 07-test.sh
@@ -4,6 +4,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 
+SOURCE_DIR="/home/ec2-user/shell-files"
+
 LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
@@ -24,4 +26,6 @@ fi
 CHECK_ROOT
 
 echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name . -mtime +14)
 
