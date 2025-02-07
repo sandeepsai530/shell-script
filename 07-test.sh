227d964ad5e8a25d62efb67147f3fa6c63209613
sai sandeep <sandeepsai530@gmail.com>
2025-02-07 19:20:32 +0530

    18 script added

--- 07-test.sh
+++ 07-test.sh
@@ -2,46 +2,11 @@
 
 USERID=$(id -u)
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-SOURCE_DIR="/home/ec2-user/app-logs"
-
-LOGS_FOLDER="/var/log/shellscript-logs"
-LOG_FILE=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 ... $R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N"
-    fi
-}
-
 CHECK_ROOT(){
-    if [ USERID -ne 0 ]
-    then
-        echo -e "$2 ... $R NOT A ROOT USER $N"
-        exit 1
-    else
-        echo -e "$2 ... $R ROOT USER $N"
-    fi
+if [ $USERID -ne 0 ]
+then
+    echo "NOT A ROOT USER"
+else
+    echo "ROOT USER"
+fi
 }
-
-echo " script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
-
-FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
-echo "files to be deleted: $FILES_TO_DELETE"
-
-
-while read -r file
-do
-    echo "Deleting file: $file"
-    rm -rf $file
-done <<< $FILES_TO_DELETE
